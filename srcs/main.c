23bb4d34f5fcc724b277d586a287cd02db7fcacd
Jeremy CAROL <jecarol@e1r3p15.42.fr>
2018-01-21 19:09:31 +0100

    more more

--- srcs/main.c
+++ srcs/main.c
@@ -33,6 +33,26 @@ struct winsize		ft_init(void)
 	return (sz);
 }
 
+void ft_move_it(int i, t_edit *line, char *buf, int check)
+{
+	while (i < line->max_size)
+	{
+		buf[2] = 68;
+		ft_left_arrow(buf, line);
+		i++;
+	}
+	if (check == 1)
+		i = 0;
+	if (check == 0)
+	{
+		line->cursor_pos--;
+		line->max_size--;
+	}
+	tputs(tgetstr("cd", NULL), 1, ft_pointchar);
+	ft_putstr(line->line);
+	line->cursor_pos = ft_strlen(line->line) + 2;
+}
+
 void ft_insert(char *buf, t_edit *line)
 {
 	char *tmp;
@@ -48,37 +68,13 @@ void ft_insert(char *buf, t_edit *line)
 	free (line->line);
 	line->line = ft_freejoinstr(tmp3, tmp2);
 	tmp2 = ft_strsub(line->line, line->cursor_pos - 2 , line->max_size);
-	while (i < line->max_size)
-	{
-		buf[2] = 68;
-		ft_left_arrow(buf, line);
-		i++;
-	}
-	i = 0;
-	tputs(tgetstr("cd", NULL), 1, ft_pointchar);
-	ft_putstr(line->line);
-	line->cursor_pos = ft_strlen(line->line) + 2;
+	ft_move_it(i, line, buf, 1);
 	while ((size_t)i < ft_strlen(tmp2))
 	{
 		buf[2] = 68;
 		ft_left_arrow(buf, line);
 		i++;
 	}
-}
-
-void ft_move_it(int i, t_edit *line, char *buf)
-{
-	while (i < line->max_size)
-	{
-		buf[2] = 68;
-		ft_left_arrow(buf, line);
-		i++;
-	}
-	line->cursor_pos--;
-	line->max_size--;
-	tputs(tgetstr("cd", NULL), 1, ft_pointchar);
-	ft_putstr(line->line);
-	line->cursor_pos = ft_strlen(line->line) + 2;
 }
 
 void ft_delete(char *buf, t_edit *line)
@@ -94,7 +90,7 @@ void ft_delete(char *buf, t_edit *line)
 		tmp = ft_strndup(line->line, ft_strlen(line->line) - 1);
 		free (line->line);
 		line->line = tmp;
-		ft_move_it(i, line, buf);
+		ft_move_it(i, line, buf, 0);
 	}
 	else if ((line->cursor_pos != line->max_size) && (line->cursor_pos > 2))
 	{
@@ -103,7 +99,7 @@ void ft_delete(char *buf, t_edit *line)
 				(ft_strlen(line->line) - line->cursor_pos) + 3);
 		free (line->line);
 		line->line = ft_freejoinstr(tmp, tmp2);
-		ft_move_it(i, line, buf);
+		ft_move_it(i, line, buf, 0);
 		while ((size_t)i < ft_strlen(tmp2))
 		{
 			buf[2] = 68;
