bc2f7f7ca13c1be70de056be4488aea4367e96a9
Remi FABRE <remifabre.rf@gmail.com>
2018-04-30 16:20:05 +0200

    fix external copy/paste

--- srcs/line_edit_tools.c
+++ srcs/line_edit_tools.c
@@ -6,7 +6,7 @@
 /*   By: rfabre <rfabre@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/27 19:11:32 by rfabre            #+#    #+#             */
-/*   Updated: 2018/04/27 19:14:39 by rfabre           ###   ########.fr       */
+/*   Updated: 2018/04/30 16:19:04 by rfabre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ void			ft_move_it(t_edit *line, int check)
 void			print_cpy(int buf, t_edit *line)
 {
 	char		str[5];
-	char		tmp[2];
 	int			i;
 
 	i = 0;
@@ -72,9 +71,8 @@ void			print_cpy(int buf, t_edit *line)
 	str[4] = '\0';
 	while (str[i])
 	{
-		tmp[0] = str[i];
-		tmp[1] = '\0';
-		add_to_line(line, (int)str[i]);
+		if (ft_isprint(str[i]))
+			add_to_line(line, (int)str[i]);
 		i++;
 	}
 }
@@ -91,9 +89,9 @@ int				check_copy(int buf)
 	check[3] = buf >> 24;
 	while (i < 4)
 	{
-		if ((int)check[i] > 126 || (int)check[i] < 32)
-			return (0);
+		if ((int)check[i] < 127 && (int)check[i] > 31)
+			return (1);
 		i++;
 	}
-	return (1);
+	return (0);
 }
